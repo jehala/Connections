7608f1e3f2361976e3a92dcb21dcec72dc7df09d
jehala <jennifer.ehala@gmail.com>
2021-10-20 19:43:40 -0400

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// var inputs = [
+//     document.querySelector('#user-2'),
+//     document.querySelector('#user-1') ];
+
+
+function remove1() {
+    document.querySelector('#user-2').remove();
+    document.querySelector('#user-1').remove();
+}
+
+
+
+function remove2() {
+    document.querySelector('#user-3').remove();
+    document.querySelector('#user-4').remove();
+}
+
+
+
+
+var janeDoe = document.getElementById("nameID");
+function rename() {
+    janeDoe.innerText = "Aubrey Hepburn";
+}
